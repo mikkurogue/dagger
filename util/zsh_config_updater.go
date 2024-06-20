5ed4196f859a71bc67cec9e305e7f5ae93ef8da2
Michael Lindemans <michaellindemans@mw-h73chyqfnl.home>
2024-06-20 11:29:10 +0200

    add example string command so i dont forget

--- util/zsh_config_updater.go
+++ util/zsh_config_updater.go
@@ -8,6 +8,8 @@ import (
 	"github.com/fatih/color"
 )
 
+// example alias:  alias := "\n# Added by Go program\nalias ll='ls -la'\n"
+
 func ZshConfigUpdater(alias string) {
 	usr, err := user.Current()
 	if err != nil {

83bc08f4777f362738f140b9238d7c222f7edf8a
Mikku <michael.lindemans@outlook.com>
2024-06-23 23:22:31 +0200

    add that we can write something to the cfg file

--- config/config.go
+++ config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -10,26 +11,34 @@ import (
 )
 
 const (
-	config_file_dir  = "~/.dagger"
+	config_file_dir  = "/.dagger"
 	config_file_name = ".cfg"
-	config_file_path = "~/.dagger/.cfg"
+	config_file_path = "/.dagger/.cfg"
 )
 
 // contents of the cfg
 type Config struct {
-	path string
-
-	cli_tools   *[]string
-	langs       *[]string
-	aliases     *[]string
-	code_editor *string
+	cli_tools   []string
+	langs       []string
+	aliases     []string
+	code_editor string
 }
 
 func (c *Config) ReadConfig() {
 
-	if _, err := os.Stat(config_file_path); err != nil {
+	user_home_dir, err := os.UserHomeDir()
+	if err != nil {
 		fmt.Println(lipgloss.NewStyle().
-			Background(lipgloss.Color("orange")).
+			Background(lipgloss.Color("#ff0000")).
+			Foreground(lipgloss.Color("#d3d3d3")).
+			Italic(true).
+			Padding(0, 1).
+			Render(err.Error()))
+	}
+
+	if _, err := os.Stat(user_home_dir + config_file_path); err != nil {
+		fmt.Println(lipgloss.NewStyle().
+			Background(lipgloss.Color("#ff0000")).
 			Foreground(lipgloss.Color("#d3d3d3")).
 			Italic(true).
 			Padding(0, 1).
@@ -39,25 +48,49 @@ func (c *Config) ReadConfig() {
 }
 
 func OpenConfig(c *Config) {
-	config, err := os.OpenFile(c.path, os.O_RDONLY, 0644)
+	user_home_dir, err := os.UserHomeDir()
 	if err != nil {
 		fmt.Println(lipgloss.NewStyle().
-			Background(lipgloss.Color("orange")).
+			Background(lipgloss.Color("#ff0000")).
+			Foreground(lipgloss.Color("#d3d3d3")).
+			Italic(true).
+			Padding(0, 1).
+			Render(err.Error()))
+	}
+
+	fmt.Println(user_home_dir + config_file_path)
+
+	config, err := os.OpenFile(user_home_dir+config_file_path, os.O_RDONLY, 0644)
+	if err != nil {
+		fmt.Println(lipgloss.NewStyle().
+			Background(lipgloss.Color("#ff0000")).
 			Foreground(lipgloss.Color("#d3d3d3")).
 			Italic(true).
 			Padding(0, 1).
 			Render("Could not open file... double check directory otherwise nuke"))
 
-		c.path = config_file_path
-		c.aliases = nil
-		c.cli_tools = nil
-		c.code_editor = nil
-		c.langs = nil
+		c.aliases = []string{"a1", "a2"}
+		c.cli_tools = []string{"tool1", "tool2"}
+		c.code_editor = "default_editor"
+		c.langs = []string{"lang1", "lang2"}
 
 		Create(c)
+		return
+	}
+	defer config.Close()
+
+	content, err := io.ReadAll(config)
+	if err != nil {
+		fmt.Println(lipgloss.NewStyle().
+			Background(lipgloss.Color("#ff0000")).
+			Foreground(lipgloss.Color("#d3d3d3")).
+			Italic(true).
+			Padding(0, 1).
+			Render("giga err", err.Error()))
+		return
 	}
 
-	fmt.Println(config)
+	fmt.Println("config content:\n", string(content))
 }
 
 func Create(config *Config) {
@@ -86,8 +119,9 @@ func Create(config *Config) {
 	}
 
 	// Create ~/.dagger/.cfg file
-	cfgFile := filepath.Join(daggerDir, ".cfg")
-	if _, err := os.Create(cfgFile); err != nil {
+	cfg_file := filepath.Join(daggerDir, ".cfg")
+	file, err := os.OpenFile(cfg_file, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
 		fmt.Println(lipgloss.NewStyle().
 			Background(lipgloss.Color("#ff0000")).
 			Foreground(lipgloss.Color("#d3d3d3")).
@@ -96,11 +130,42 @@ func Create(config *Config) {
 			Render("Error creating ~/.dagger/.cfg file:" + err.Error()))
 		return
 	}
+	defer file.Close()
 
 	// TODO Write the defaults here
+	//
+	content := fmt.Sprintf(
+		"# dagger config\nAliases: %v\nCLI Tools: %v\nCode Editor: %s\nLangs: %v\n",
+		config.aliases,
+		config.cli_tools,
+		config.code_editor,
+		config.langs,
+	)
+
+	if _, err := file.WriteString(content); err != nil {
+		fmt.Println(lipgloss.NewStyle().
+			Background(lipgloss.Color("#ff0000")).
+			Foreground(lipgloss.Color("#d3d3d3")).
+			Italic(true).
+			Padding(0, 1).
+			Render("Error writing to ~/.dagger/.cfg file:" + err.Error()))
+		return
+	}
+
+	fmt.Println("Configuration file created with default values")
 }
 
 func (c *Config) DeleteConfig() {
 	// allow user to delete the config just incase it somehow corrupts
-	exec.Command("rm", "-rf", c.path)
+	//
+	user_home_dir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println(lipgloss.NewStyle().
+			Background(lipgloss.Color("#ff0000")).
+			Foreground(lipgloss.Color("#d3d3d3")).
+			Italic(true).
+			Padding(0, 1).
+			Render(err.Error()))
+	}
+	exec.Command("rm", "-rf", user_home_dir+config_file_path)
 }

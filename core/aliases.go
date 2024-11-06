dd11f4ed9287b0658135759b4878baa2f8d84e8c
Mikku <michael.lindemans@outlook.com>
2024-11-06 16:04:40 +0100

    add neovim and helix to installer, also update to add a few handy ali… (#16)
    add neovim and helix to installer, also update to add a few handy aliases i use daily
    
    Co-authored-by: Michael Lindemans <m.lindemans@bigmile.eu>

--- core/aliases.go
+++ core/aliases.go
@@ -5,13 +5,15 @@ import (
 )
 
 // validation shouldnt be necessary as these aliases arent required.
-
 func Aliases(aliases *[]string) *huh.Group {
 	return huh.NewGroup(huh.NewMultiSelect[string]().
 		Title("Aliases").
 		Description("Select the aliases you would like to install").
 		Options(
 			huh.NewOption("git purge", "git-purge"),
+			huh.NewOption("git merge develop", "gmd"),
+			huh.NewOption("git push", "gp"),
+			huh.NewOption("cd back", ".."),
 			huh.NewOption("Skip step", "skip"),
 		).
 		Value(aliases))
